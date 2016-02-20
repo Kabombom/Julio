1577dbcbee2dabbdbc11f20a3f07b58ddb9d2615
Miguel Machado <mantonio@student.dei.uc.pt>
2016-02-20 03:50:12 +0000

    :raccon:

--- app/src/main/java/com/example/julio/julio/Form.java
+++ app/src/main/java/com/example/julio/julio/Form.java
@@ -1,11 +1,16 @@
 package com.example.julio.julio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import io.github.kexanie.library.MathView;
 
 public class Form extends AppCompatActivity {
 
@@ -16,6 +21,14 @@ public class Form extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button button = (Button) findViewById(R.id.Text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CheatSheetActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }

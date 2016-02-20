9354a9455777d9e9a4193fd989372a1827554e01
António Lima <tocalagarto93@gmail.com>
2016-02-20 16:23:56 +0000

    :dickbutt:

--- app/src/main/java/com/example/julio/julio/TeXEditorActivity.java
+++ app/src/main/java/com/example/julio/julio/TeXEditorActivity.java
@@ -1,15 +1,31 @@
 package com.example.julio.julio;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import io.github.kexanie.library.MathView;
 
 public class TeXEditorActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int RESULT_LOAD_IMAGE = 1999;
+
+    private Bitmap photo_bitmap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +55,96 @@ public class TeXEditorActivity extends AppCompatActivity {
 
         ((EditText) findViewById(R.id.formula_tex)).addTextChangedListener(inputTextWatcher);
 
+        Button takePhotoButton = (Button) findViewById(R.id.take_photo);
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFromCamera(v);
+            }
+        });
+
+        Button loadImageButton = (Button) findViewById(R.id.load_image);
+        loadImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFromGallery(v);
+            }
+        });
+
+        /*
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        DisplayImageOptions options = new DisplayImageOptions.Builder().cacheInMemory(true)
+                .cacheOnDisc(true).resetViewBeforeLoading(true)
+                .build();
+
+        imageLoader.displayImage(src, imageView, options);
+        */
+
+    }
+
+    public void getFromCamera(View view) {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    private void displayPreview() {
+        ImageView imageView = (ImageView) findViewById(R.id.tex_image_preview);
+        imageView.setImageBitmap(photo_bitmap);
+
+    }
+
+    public void getFromGallery(View view) {
+        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST) {
+                photo_bitmap = (Bitmap) data.getExtras().get("data");
+
+                displayPreview();
+            }
+            else if (requestCode == RESULT_LOAD_IMAGE) {
+                try {
+                    Bitmap bitmap = decodeUri(data.getData());
+                    if(bitmap != null) {
+                        photo_bitmap = bitmap;
+                        displayPreview();
+                    }
+                    else
+                        Toast.makeText(getBaseContext(), "Oops...", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    Toast.makeText(getBaseContext(), "Oops...", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private Bitmap decodeUri(Uri selectedImage) throws FileNotFoundException {
+        BitmapFactory.Options o = new BitmapFactory.Options();
+        o.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(
+                getContentResolver().openInputStream(selectedImage), null, o);
+
+        final int REQUIRED_SIZE = 960;
+
+        int width_tmp = o.outWidth, height_tmp = o.outHeight;
+        int scale = 1;
+        while (true) {
+            if (width_tmp / 2 < REQUIRED_SIZE || height_tmp / 2 < REQUIRED_SIZE) {
+                break;
+            }
+            width_tmp /= 2;
+            height_tmp /= 2;
+            scale *= 2;
+        }
+
+        BitmapFactory.Options o2 = new BitmapFactory.Options();
+        o2.inSampleSize = scale;
+        return BitmapFactory.decodeStream(
+                getContentResolver().openInputStream(selectedImage), null, o2);
     }
 
 }

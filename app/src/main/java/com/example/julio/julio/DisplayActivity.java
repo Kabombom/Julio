485cfebb284da12f11193faa3e82a25f045f6cda
pedrocb <pedrocb_c-s@hotmail.com>
2016-02-20 17:26:03 +0000

    :monkey:

--- app/src/main/java/com/example/julio/julio/DisplayActivity.java
+++ app/src/main/java/com/example/julio/julio/DisplayActivity.java
@@ -1,8 +1,11 @@
 package com.example.julio.julio;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class DisplayActivity extends AppCompatActivity {
 
@@ -17,6 +20,14 @@ public class DisplayActivity extends AppCompatActivity {
         Section section = (Section)b.getSerializable("Section");
         getSupportActionBar().setTitle(section.title);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.editButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TeXEditorActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }

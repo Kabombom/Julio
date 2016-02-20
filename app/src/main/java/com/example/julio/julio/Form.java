d0029cce2a52d800f929da2d395d864b95a01ab5
António Lima <tocalagarto93@gmail.com>
2016-02-20 14:40:47 +0000

    :moose:

--- app/src/main/java/com/example/julio/julio/Form.java
+++ app/src/main/java/com/example/julio/julio/Form.java
@@ -1,36 +1,25 @@
 package com.example.julio.julio;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.os.Debug;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-
-import io.github.kexanie.library.MathView;
-
+import android.widget.ImageView;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
-import java.util.Set;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class Form extends AppCompatActivity {
 
@@ -47,6 +36,29 @@ public class Form extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), TeXEditorActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        final Context context = this;
+
+        final ImageView imageView = (ImageView) findViewById(R.id.section_color);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AmbilWarnaDialog dialog = new AmbilWarnaDialog(context, ((ColorDrawable) imageView.getBackground()).getColor(), new AmbilWarnaDialog.OnAmbilWarnaListener() {
+                    @Override
+                    public void onOk(AmbilWarnaDialog dialog, int color) {
+                        // color is the color selected by the user.
+                        imageView.setBackgroundColor(color);
+                    }
+
+                    @Override
+                    public void onCancel(AmbilWarnaDialog dialog) {
+                        // cancel was selected by the user
+                    }
+
+                });
+                dialog.show();
             }
         });
 

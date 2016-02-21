895b527619ae71c9825266a5fbd9b3a09b677432
António Lima <tocalagarto93@gmail.com>
2016-02-21 02:31:55 +0000

    :meerkat:

--- app/src/main/java/com/example/julio/julio/DisplayActivity.java
+++ app/src/main/java/com/example/julio/julio/DisplayActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,7 +16,7 @@ public class DisplayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display);
+        setContentView(R.layout.activity_start_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,7 +24,7 @@ public class DisplayActivity extends AppCompatActivity {
         Section section = (Section)b.getSerializable("Section");
         getSupportActionBar().setTitle(section.title);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.editButton);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.addButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
